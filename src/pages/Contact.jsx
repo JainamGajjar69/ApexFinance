aa71577ea2314c532cd4e17f438dbd9c5166f7b1
JainamGajjar69 <thefenixa30@gmail.com>
2023-04-21 02:27:03 +0530

    added backend service for contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,52 @@
+import React, { useState } from "react";
 import "./Contact.css";
+
 export default function Contact() {
+  const [userData, setUserData] = useState({
+    yourName: "",
+    emailAddress: "",
+    yourMessage: "",
+  });
+  let name, value;
+  const postUserData = (event) => {
+    name = event.target.name;
+    value = event.target.value;
+    setUserData({ ...userData, [name]: value });
+  };
+
+  //connect with firebase
+  const submitData = async (event) => {
+    event.preventDefault();
+    const { yourName, emailAddress, yourMessage } = userData;
+
+    if (yourName && emailAddress && yourMessage) {
+      const res = await fetch(
+        "https://apexfinanceuserdata-default-rtdb.firebaseio.com/userDatabase.json",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            yourName,
+            emailAddress,
+            yourMessage,
+          }),
+        }
+      );
+      if (res) {
+        setUserData({ yourName: "", emailAddress: "", yourMessage: "" });
+        alert("Data Stored");
+      } else {
+        alert("Fill the data properly");
+      }
+    } else {
+      alert("Fill the data properly");
+    }
+  };
+
+
+
   return (
     <>
       <div class="section wf-section snipcss-V4UWm">
@@ -18,7 +65,7 @@ export default function Contact() {
                     id="wf-form-Contact-Form"
                     name="wf-form-Contact-Form"
                     data-name="Contact Form"
-                    method="get"
+                    method="POST"
                     class="form-vertical"
                     aria-label="Contact Form"
                   >
@@ -27,37 +74,43 @@ export default function Contact() {
                         type="text"
                         class="form-input form-input-large w-input"
                         maxlength="256"
-                        name="name"
+                        name="yourName"
                         data-name="Name"
                         placeholder="Your Name"
                         id="contact-name"
                         required=""
+                        value={userData.yourName}
+                        onChange={postUserData}
                       />
                       <input
                         type="email"
                         class="form-input form-input-large w-input"
                         maxlength="256"
-                        name="email"
+                        name="emailAddress"
                         data-name="Email"
                         placeholder="Email Address"
                         id="contact-email"
                         required=""
+                        value={userData.emailAddress}
+                        onChange={postUserData}
                       />
                     </div>
                     <textarea
                       placeholder="Your Message"
                       maxlength="5000"
                       id="Message"
-                      name="Message"
+                      name="yourMessage"
                       data-name="Message"
                       class="form-input textarea w-input"
+                      value={userData.yourMessage}
+                      onChange={postUserData}
                     ></textarea>
                     <input
                       type="submit"
-                      value="Send Message"
                       data-wait="Please wait..."
                       id="w-node-_536b9db9-8d9a-5940-b59e-6577ca86cd40-bcf9e2c6"
                       class="button-large w-button"
+                      onClick={submitData}
                     />
                   </form>
                   <div
@@ -93,7 +146,7 @@ export default function Contact() {
                     />
                   </div>
                   <h4>Email</h4>
-                  <div class="text-lead" >
+                  <div class="text-lead">
                     <a href="mailto:hello@panels.io?subject=Website%20Enquiry">
                       apexfinance@gmail.com
                     </a>
