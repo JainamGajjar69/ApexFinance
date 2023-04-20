375bc5c33d4e4244c09f6961a42a0fc000369ebb
JainamGajjar69 <thefenixa30@gmail.com>
2023-04-21 00:56:11 +0530

    changed content for about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,152 +1,142 @@
-import "./About.css"
-export default function About(){
-    return (
-      <>
-        <div class="section wf-section snipcss-16kPC">
-          <div
-            data-w-id="b7800c70-6a12-e03d-3b66-3f490ee5bafd"
-            class="container style-g47sa"
-            id="style-g47sa"
-          >
-            <div class="w-layout-grid hero-2-grid">
-              <div class="hero-content">
-                <h2 class="display-heading-4">
-                  <strong>RAJVI PANCHAL</strong>
-                </h2>
-                <div class="space-bottom">
-                  <h3 class="display-inline text-gray-4 smaller">
-                    <strong class="display-inline text-gray-4 smaller">
-                      Will be helping in the Backend development of the website
-                      and calculator.
-                    </strong>
-                  </h3>
-                </div>
+import "./About.css";
+export default function About() {
+  return (
+    <>
+      <div className="section wf-section snipcss-16kPC">
+        <div className="container style-g47sa">
+          <div className="hero-2-grid">
+            <div className="hero-content">
+              <h2 className="display-heading-4">
+                <strong>Jainam Gajjar</strong>
+              </h2>
+              <div className="space-bottom">
+                <h3 className="display-inline text-gray-4 smaller">
+                  <strong className="display-inline text-gray-4 smaller">
+                    Project leader, Helping mainly in Website Building Frontend
+                    and Backend Respectively.
+                  </strong>
+                </h3>
               </div>
-              <div
-                id="w-node-_01c9d81d-a50c-9dc1-f213-f87df907a24c-12f9e2a1"
-                class="panel-body bg-gradient-3 rounded"
-              >
-                <img src="" width="700" alt="" class="rounded-image"></img>
-              </div>
+            </div>
+            <div className="panel-body bg-gradient-3 rounded">
+              <img
+                src=""
+                alt=""
+                className="rounded-image"
+                style={{ maxWidth: "100%", height: "auto" }}
+              />
             </div>
           </div>
         </div>
-        <div class="section wf-section snipcss-16kPC">
-          <div
-            data-w-id="b7800c70-6a12-e03d-3b66-3f490ee5bafd"
-            class="container style-g47sa"
-            id="style-g47sa"
-          >
-            <div class="w-layout-grid hero-2-grid">
-              <div class="hero-content">
-                <h2 class="display-heading-4">
-                  <strong>MEHA BAGADIA</strong>
-                </h2>
-                <div class="space-bottom">
-                  <h3 class="display-inline text-gray-4 smaller">
-                    <strong class="display-inline text-gray-4 smaller">
-                      Will be helping in the project by creating a budget of the
-                      institution, and allocation of funds.
-                    </strong>
-                  </h3>
-                </div>
+      </div>
+      <div className="section wf-section snipcss-16kPC">
+        <div className="container style-g47sa">
+          <div className="hero-2-grid">
+            <div className="hero-content">
+              <h2 className="display-heading-4">
+                <strong>RAJVI PANCHAL</strong>
+              </h2>
+              <div className="space-bottom">
+                <h3 className="display-inline text-gray-4 smaller">
+                  <strong className="display-inline text-gray-4 smaller">
+                    Will be helping in the Backend development of the website
+                    and calculator.
+                  </strong>
+                </h3>
               </div>
-              <div
-                id="w-node-_01c9d81d-a50c-9dc1-f213-f87df907a24c-12f9e2a1"
-                class="panel-body bg-gradient-3 rounded"
-              >
-                <img src="" width="700" alt="" class="rounded-image"></img>
-              </div>
+            </div>
+            <div className="panel-body bg-gradient-3 rounded">
+              <img
+                src=""
+                alt=""
+                className="rounded-image"
+                style={{ maxWidth: "100%", height: "auto" }}
+              />
             </div>
           </div>
         </div>
-        <div class="section wf-section snipcss-16kPC">
-          <div
-            data-w-id="b7800c70-6a12-e03d-3b66-3f490ee5bafd"
-            class="container style-g47sa"
-            id="style-g47sa"
-          >
-            <div class="w-layout-grid hero-2-grid">
-              <div class="hero-content">
-                <h2 class="display-heading-4">
-                  <strong>KINJAL PATIL</strong>
-                </h2>
-                <div class="space-bottom">
-                  <h3 class="display-inline text-gray-4 smaller">
-                    <strong class="display-inline text-gray-4 smaller">
-                      Will be helping in the Frontend development of the website
-                      and make navigation throughout the website easier
-                    </strong>
-                  </h3>
-                </div>
+      </div>
+      <div className="section wf-section snipcss-16kPC">
+        <div className="container style-g47sa">
+          <div className="hero-2-grid">
+            <div className="hero-content">
+              <h2 className="display-heading-4">
+                <strong>Meha Bagadia</strong>
+              </h2>
+              <div className="space-bottom">
+                <h3 className="display-inline text-gray-4 smaller">
+                  <strong className="display-inline text-gray-4 smaller">
+                    Will be helping in the project by creating a budget of the
+                    institution, and allocation of funds.
+                  </strong>
+                </h3>
               </div>
-              <div
-                id="w-node-_01c9d81d-a50c-9dc1-f213-f87df907a24c-12f9e2a1"
-                class="panel-body bg-gradient-3 rounded"
-              >
-                <img src="" width="700" alt="" class="rounded-image"></img>
-              </div>
+            </div>
+            <div className="panel-body bg-gradient-3 rounded">
+              <img
+                src=""
+                alt=""
+                className="rounded-image"
+                style={{ maxWidth: "100%", height: "auto" }}
+              />
             </div>
           </div>
         </div>
-        <div class="section wf-section snipcss-16kPC">
-          <div
-            data-w-id="b7800c70-6a12-e03d-3b66-3f490ee5bafd"
-            class="container style-g47sa"
-            id="style-g47sa"
-          >
-            <div class="w-layout-grid hero-2-grid">
-              <div class="hero-content">
-                <h2 class="display-heading-4">
-                  <strong>DIVYA PARMAR</strong>
-                </h2>
-                <div class="space-bottom">
-                  <h3 class="display-inline text-gray-4 smaller">
-                    <strong class="display-inline text-gray-4 smaller">
-                      Will be handling salary allocation, organization structure
-                      and information flow of the firm.
-                    </strong>
-                  </h3>
-                </div>
+      </div>
+      <div className="section wf-section snipcss-16kPC">
+        <div className="container style-g47sa">
+          <div className="hero-2-grid">
+            <div className="hero-content">
+              <h2 className="display-heading-4">
+                <strong>KINJAL PATIL</strong>
+              </h2>
+              <div className="space-bottom">
+                <h3 className="display-inline text-gray-4 smaller">
+                  <strong className="display-inline text-gray-4 smaller">
+                    Will be helping in the Frontend development of the website
+                    and make navigation throughout the website easier
+                  </strong>
+                </h3>
               </div>
-              <div
-                id="w-node-_01c9d81d-a50c-9dc1-f213-f87df907a24c-12f9e2a1"
-                class="panel-body bg-gradient-3 rounded"
-              >
-                <img src="" width="700" alt="" class="rounded-image"></img>
-              </div>
+            </div>
+            <div className="panel-body bg-gradient-3 rounded">
+              <img
+                src=""
+                alt=""
+                className="rounded-image"
+                style={{ maxWidth: "100%", height: "auto" }}
+              />
             </div>
           </div>
         </div>
-        <div class="section wf-section snipcss-16kPC">
-          <div
-            data-w-id="b7800c70-6a12-e03d-3b66-3f490ee5bafd"
-            class="container style-g47sa"
-            id="style-g47sa"
-          >
-            <div class="w-layout-grid hero-2-grid">
-              <div class="hero-content">
-                <h2 class="display-heading-4">
-                  <strong>JAINAM GAJJAR</strong>
-                </h2>
-                <div class="space-bottom">
-                  <h3 class="display-inline text-gray-4 smaller">
-                    <strong class="display-inline text-gray-4 smaller">
-                      Project leader, Supervisor and will help in both Tech and
-                      Finance field.
-                    </strong>
-                  </h3>
-                </div>
+      </div>
+      <div className="section wf-section snipcss-16kPC">
+        <div className="container style-g47sa">
+          <div className="hero-2-grid">
+            <div className="hero-content">
+              <h2 className="display-heading-4">
+                <strong>DIVYA PARMAR</strong>
+              </h2>
+              <div className="space-bottom">
+                <h3 className="display-inline text-gray-4 smaller">
+                  <strong className="display-inline text-gray-4 smaller">
+                    Will be handling salary allocation, organization structure
+                    and information flow of the firm.
+                  </strong>
+                </h3>
               </div>
-              <div
-                id="w-node-_01c9d81d-a50c-9dc1-f213-f87df907a24c-12f9e2a1"
-                class="panel-body bg-gradient-3 rounded"
-              >
-                <img src="" width="700" alt="" class="rounded-image"></img>
-              </div>
+            </div>
+            <div className="panel-body bg-gradient-3 rounded">
+              <img
+                src=""
+                alt=""
+                className="rounded-image"
+                style={{ maxWidth: "100%", height: "auto" }}
+              />
             </div>
           </div>
         </div>
-      </>
-    );
+      </div>
+    </>
+  );
 }
