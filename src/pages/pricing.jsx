77bf8338da8cee0b41b31bc281a242fe38fccb9e
JainamGajjar69 <thefenixa30@gmail.com>
2023-04-21 10:49:55 +0530

    added pricing page content

--- src/pages/pricing.jsx
+++ src/pages/pricing.jsx
@@ -1,9 +1,60 @@
-import './pricing.css'
+import "./pricing.css";
 export default function Pricing() {
-    return <>
-    <div className="pricing-head">
-    <h1> This where we will show our content pricing!!!!!</h1>
-    </div>
-    </>
+  return (
+    <>
+      <header>
+        <div className="head">
+          <h1>Apex Nifty SDL debt fund</h1>
+        </div>
+      </header>
 
+      <div className="Table">
+        <body>
+          <h2>
+            1.12% <h6>return</h6>
+          </h2>
+          <h3>
+            NAV 20 April 2023
+            <table>
+              <tr>
+                <td>₹10.56</td>
+              </tr>
+            </table>
+          </h3>
+          <div className="p">
+            <h6>Purchase with subscription(1 Year)</h6>
+            <h6>Purchase with subscription(5 Year)</h6>
+          </div>
+          <div class="align-content-left  ">
+            <a
+              href="/request-demo"
+              class="button-large w-button snipcss0-5-8-9"
+            >
+              Purchase
+            </a>
+            <a
+              href="/request-demo"
+              class="button-large w-button snipcss0-5-8-9"
+            >
+              Purchase
+            </a>
+            <a
+              href="/request-demo"
+              class="button-large w-button snipcss0-5-8-9"
+            >
+              Purchase
+            </a>
+          </div>
+          {/* <div class="align-content-left  w-container snipcss0-4-4-8 ">
+            <a
+              href="/request-demo"
+              class="button-large w-button snipcss0-5-8-9"
+            >
+              Purchase
+            </a>
+          </div> */}
+        </body>
+      </div>
+    </>
+  );
 }
