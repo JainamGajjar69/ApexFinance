799e543029cf5e7a28113e33e522a38c333aab2f
JainamGajjar69 <thefenixa30@gmail.com>
2023-04-29 15:30:50 +0530

    did so many changes in navbar homepage and etc

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,27 +1,28 @@
 /* eslint-disable jsx-a11y/anchor-has-content */
+
 import React from "react";
 import "./Home.css";
 
 export default function Home() {
   return (
     <>
-      <div class="section hero-section wf-section snipcss0-0-0-1 snipcss-qoDRg">
-        <div class="container snipcss0-1-1-2">
-          <div class="w-layout-grid hero-1-grid snipcss0-2-2-3">
-            <div class="snipcss0-3-3-4">
-              <h1 class="display-heading-2 snipcss0-4-4-5">
+      <div className="section hero-section wf-section snipcss0-0-0-1 snipcss-qoDRg">
+        <div className="container snipcss0-1-1-2">
+          <div className="w-layout-grid hero-1-grid snipcss0-2-2-3">
+            <div className="snipcss0-3-3-4">
+              <h1 className="display-heading-2 snipcss0-4-4-5">
                 You have problem with your finances ? don't worry now you have
                 Apex Finance
               </h1>
-              <div class="space-bottom snipcss0-4-4-6">
-                <h3 class="display-inline text-gray-4 snipcss0-5-6-7">
+              <div className="space-bottom snipcss0-4-4-6">
+                <h3 className="display-inline text-gray-4 snipcss0-5-6-7">
                   "Because we think Money comes and goes with Emotion"
                 </h3>
               </div>
-              <div class="align-content-left w-container snipcss0-4-4-8">
+              <div className="align-content-left w-container snipcss0-4-4-8">
                 <a
-                  href="/request-demo"
-                  class="button-large w-button snipcss0-5-8-9"
+                  href="/contact"
+                  className="button-large w-button snipcss0-5-8-9"
                 >
                   Request demo
                 </a>
@@ -29,17 +30,17 @@ export default function Home() {
             </div>
             <div
               id="w-node-_4b855073-eb53-565a-9f40-9639baac7336-12f9e2a1"
-              class="panel-body bg-gradient-6 rounded w-embed w-iframe snipcss0-3-3-10"
+              className="panel-body bg-gradient-6 rounded w-embed w-iframe snipcss0-3-3-10"
             >
-              <div class="snipcss0-4-10-11 style-F2RwW" id="style-F2RwW">
+              <div className="snipcss0-4-10-11 style-F2RwW" id="style-F2RwW">
                 <iframe
                   width="560"
                   height="315"
                   src="https://www.youtube.com/embed/gFQNPmLKj1k"
                   title="YouTube video player"
-                  frameborder="0"
+                  frameBorder="0"
                   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                  allowfullscreen
+                  allowFullScreen
                 ></iframe>
               </div>
             </div>
@@ -48,72 +49,122 @@ export default function Home() {
       </div>
 
       {/* mid text */}
-      <div class="section no-bottom-section wf-section snipcss-SSyZH">
+      <div className="section no-bottom-section wf-section snipcss-SSyZH">
         <div
           data-w-id="a2f9dfdb-2070-4147-d5a1-35705e7a3a9a"
-          class="container justify-content-center style-xfLAJ"
+          className="container justify-content-center style-xfLAJ"
           id="style-xfLAJ"
         >
-          <div class="center-content content-width-large tether-target-attached-top tether-element-attached-top tether-element-attached-center tether-target-attached-center">
-            <h2 class="display-heading-3">How it works</h2>
+          <div className="center-content content-width-large tether-target-attached-top tether-element-attached-top tether-element-attached-center tether-target-attached-center">
+            <h2 className="display-heading-3">How it works</h2>
           </div>
         </div>
       </div>
 
       {/* mid */}
-      <div class="section wf-section snipcss-Q55Hv">
+      {/* <div className="section wf-section snipcss-Q55Hv">
         <div
           data-w-id="ec9510b8-26cf-9625-0e12-60d055382d89"
-          class="container grid-container style-WMjrW"
+          className="container grid-container style-WMjrW"
           id="style-WMjrW"
         >
           <div
             data-w-id="ec9510b8-26cf-9625-0e12-60d055382d8a"
-            class="w-layout-grid grid-thirds space-bottom style-FCBnL"
+            className="w-layout-grid grid-thirds space-bottom style-FCBnL"
             id="style-FCBnL"
           >
-            <div class="panel">
-              <div class="panel-body">
-                <div class="space-bottom">
-                  <div class="circle-large bg-primary-8">
-                    <h3 class="large-heading no-bottom-space">1</h3>
+            <div className="panel">
+              <div className="panel-body">
+                <div className="space-bottom">
+                  <div className="circle-large bg-primary-8">
+                    <h3 className="large-heading no-bottom-space">1</h3>
                   </div>
                 </div>
                 <div>
-                  <div class="space-bottom-large">
-                    <h3 class="card-heading larger">Mutual Funds</h3>
-                    <div class="card-heading text-gray-4 display-inline"></div>
+                  <div className="space-bottom-large">
+                    <h3 className="card-heading larger">Mutual Funds</h3>
+                    <div className="card-heading text-gray-4 display-inline"></div>
                   </div>
                 </div>
               </div>
             </div>
-            <div class="panel">
-              <div class="panel-body">
-                <div class="space-bottom">
-                  <div class="circle-large bg-primary-3">
-                    <h3 class="large-heading no-bottom-space">2</h3>
+            <div className="panel">
+              <div className="panel-body">
+                <div className="space-bottom">
+                  <div className="circle-large bg-primary-3">
+                    <h3 className="large-heading no-bottom-space">2</h3>
                   </div>
                 </div>
                 <div>
-                  <div class="space-bottom-large">
-                    <h3 class="card-heading larger">Budget Friendly </h3>
-                    <div class="card-heading text-gray-4 display-inline"></div>
+                  <div className="space-bottom-large">
+                    <h3 className="card-heading larger">Budget Friendly </h3>
+                    <div className="card-heading text-gray-4 display-inline"></div>
                   </div>
                 </div>
               </div>
             </div>
-            <div class="panel">
-              <div class="panel-body">
-                <div class="space-bottom">
-                  <div class="circle-large bg-primary-1 smaller-circles">
-                    <h3 class="large-heading no-bottom-space">3</h3>
+            <div className="panel">
+              <div className="panel-body">
+                <div className="space-bottom">
+                  <div className="circle-large bg-primary-1 smaller-circles">
+                    <h3 className="large-heading no-bottom-space">3</h3>
                   </div>
                 </div>
                 <div>
-                  <div class="space-bottom-large">
-                    <h3 class="card-heading larger">Regular Income</h3>
-                    <div class="card-heading text-gray-4 display-inline"></div>
+                  <div className="space-bottom-large">
+                    <h3 className="card-heading larger">Regular Income</h3>
+                    <div className="card-heading text-gray-4 display-inline"></div>
                   </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div> */}
+      <div className="section wf-section snipcss-Q55Hv">
+        <div className="container grid-container style-WMjrW" id="style-WMjrW">
+          <div
+            className="w-layout-grid grid-thirds space-bottom style-FCBnL"
+            id="style-FCBnL"
+          >
+            <div className="panel">
+              <div className="panel-body">
+                <div className="space-bottom">
+                  <div className="circle-large bg-primary-8">
+                   
+                    <h3 className="large-heading no-bottom-space">1</h3>
+                  </div>
+                  </div>
+                
+                <div className="space-bottom-large">
+                  <h3 className="card-heading larger">Mutual Funds</h3>
+                  <div className="card-heading text-gray-4 display-inline"></div>
+                </div>
+              </div>
+            </div>
+            <div className="panel">
+              <div className="panel-body">
+                <div className="space-bottom">
+                  <div className="circle-large bg-primary-3">
+                    <h3 className="large-heading no-bottom-space">2</h3>
+                  </div>
+                </div>
+                <div className="space-bottom-large">
+                  <h3 className="card-heading larger">Budget Friendly</h3>
+                  <div className="card-heading text-gray-4 display-inline"></div>
+                </div>
+              </div>
+            </div>
+            <div className="panel">
+              <div className="panel-body">
+                <div className="space-bottom">
+                  <div className="circle-large bg-primary-1 smaller-circles">
+                    <h3 className="large-heading no-bottom-space">3</h3>
+                  </div>
+                </div>
+                <div className="space-bottom-large">
+                  <h3 className="card-heading larger">Regular Income</h3>
+                  <div className="card-heading text-gray-4 display-inline"></div>
                 </div>
               </div>
             </div>
